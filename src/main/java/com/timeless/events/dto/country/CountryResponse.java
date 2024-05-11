81515afffdb539ad4f80c9927b0cdc9c7254adc6
oBraganca <thayllonryan17@gmail.com>
2024-05-11 20:13:47 -0300

    FEAT: Set get all country using JPA QUERY

--- src/main/java/com/timeless/events/dto/country/CountryResponse.java
+++ src/main/java/com/timeless/events/dto/country/CountryResponse.java
@@ -2,7 +2,11 @@ package com.timeless.events.dto.country;
 
 import org.antlr.v4.runtime.misc.NotNull;
 
+import java.util.UUID;
+
 public class CountryResponse {
+    @NotNull
+    private UUID id;
     @NotNull
     private String name;
     @NotNull
--- src/main/java/com/timeless/events/repository/ICountryRepository.java
+++ src/main/java/com/timeless/events/repository/ICountryRepository.java
@@ -1,15 +1,21 @@
 package com.timeless.events.repository;
 
+import com.timeless.events.dto.country.CountryResponse;
 import com.timeless.events.model.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ICountryRepository extends JpaRepository<Country, UUID> {
     Optional<Country> findById(UUID id);
+
+    @Query("SELECT new com.timeless.events.dto.country.CountryResponse(c.id, c.name, c.phone_country_code) FROM country c")
+    List<CountryResponse> findAllCountryDTO();
     public <S extends Country.CountryBuilder> S save(S entity);
     Country findByName(String name);
     Country findByPhoneCountryCode(Integer phoneCountryCode);
